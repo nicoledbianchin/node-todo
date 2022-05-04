273b59c50747ce1c11c7702070bf9e2331d1d2ac
nicoledbianchin <nicoledbianchin@gmail.com>
2022-05-04 11:28:28 -0300

    implements find todo by username and by id

---
+++ controllers/apiController.js
@@ -0,0 +1,23 @@
+var Todos = require('../models/todoModel');
+var bodyParser = require('body-parser');
+
+module.exports = function (app) {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/api/todos/:uname', function (req, res) {
+        Todos.find({ username: req.params.uname }, function (err, todos) {
+            if (err) throw err;
+
+            res.send(todos);
+        });
+    });
+
+    app.get('api/todo:id', function (req, res) {
+        Todos.findById({ _id: req.params.id }, function (err, todo) {
+            if (err) throw err;
+
+            res.send(todo);
+        });
+    })
+}
